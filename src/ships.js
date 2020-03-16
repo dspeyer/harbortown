520914957cab2ab140a126b4997caa48986340d0
Daniel Speyer <dspeyer@gmail.com>
2020-03-16 13:31:22 -0400

    ship files I forgot to git add

---
+++ src/ships.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { gameState } from './gamestate.js';
+import './ship.css';
+
+export function MiniShip({ship,later}) {
+    const [mat, value] = ship;
+    let title = mat+" ship (feeds "+gameState.shipStats[mat].feed+", ships "+gameState.shipStats[mat].ship+") €"+value;
+    if (later) title += "; then "+later.map((x)=>{return "€"+x;}).join(", ");
+    return <span className="miniship" title={title} >
+             <img src={'images/'+mat+'.png'}/>
+             <span className="cost">{value}</span>
+             {later && <span className="cnt">(+{later.length})</span> }
+           </span>;
+}
+
+export function Ship({ship}) {
+    const [mat, value] = ship;
+    return <div className="ship">
+             <div className="name"><img src={'images/'+mat+'.png'}/>{mat} Ship</div>
+             <div className="capacity">
+               <div className="cost">{value}</div>
+               <div>{gameState.shipStats[mat].feed}🍪</div>
+               <div>ship {gameState.shipStats[mat].ship}</div>
+             </div>
+           </div>;
+}
